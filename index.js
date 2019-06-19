e3b8bc55841a2c61820b7dffe800f3c5bfa82aa3
Giovane Boaviagem <giovanebribeiro@gmail.com>
2019-06-19 18:02:11 -0300

    add client

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const ce = require('command-exists').sync;
 const debug = require('debug')('taglog:index.js');
+const path = require('path');
+const readPkg = require('read-pkg');
+const explorer = require('cosmiconfig')('taglog');
 
 async function execCommand(cmd){
   debug('run command: ', cmd)
@@ -16,21 +19,50 @@ async function execCommand(cmd){
   return stdout;
 }
 
-async function main(options){
+async function main(argv){
 
-  if(!options) options = {};
-  const lineFormat = options.format || '%h %s';
+  const scriptPath = argv[1];
+
+  const pkgPath = path.resolve(scriptPath.split('node_modules')[0]);
+  try{
+    readPkg.sync(pkgPath, { normalize: false }); // test if packa.json exists
+  } catch(err){
+    if(err.code !== 'ENOENT'){
+      throw err;
+    }
+  }
+
+  const options = explorer.searchSync(pkgPath) || {}; 
+  debug('options object = ', options);
+
+  const lineFormat = options.format || '* %h %s';
+  const output = options.output || 'tag'
+  const tagPrefix = options.tagPrefix || 'v';
+
+  if(!actualTag) throw new Error('The actual tag is required.');
+
+  let nLine = '\n';
+  if(process.platform === 'win32') nLine = '\r' + nLine;
   
   if(!ce('git')){
     throw new Error('Git is not installed. Please install git to use this package properly');
   }
 
-  const lastTag = await execCommand('git describe --tags --abbrev=0');
+  let lastTag = await execCommand('git describe --tags --abbrev=0');
   lastTag = lastTag.trim();
   debug('lastTag: ', lastTag);
   
   const changelog = await execCommand('git log --pretty=format:\'' + lineFormat + '\' ' + lastTag + '..HEAD');
   debug('changelog: ', changelog);
+
+  let tagMessage = '# Release Version ' + actualTag + nLine + nLine;
+  tagMessage += changelog;
+  debug('tagMessage = \n', tagMessage);
+
+  if(output === 'tag'){
+  //  await execCommand('git tag -a ' + tagPrefix + actualTag + '-m \"' + tagMessage + '\"'); 
+  }
 }
 
-main();
+module.exports = main;
+//main(null, '0.0.1');
